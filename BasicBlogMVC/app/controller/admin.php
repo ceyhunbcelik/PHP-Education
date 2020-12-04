d0c7bc727771f3681b80fbcbe43a0447e8259190
ceyhunbcelik <ceyhunbcelik@icloud.com>
2020-12-05 00:50:30 +0300

    fix repo

--- BasicBlogMVC/app/controller/admin.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-  require_once(PATH . '/app/admin/index.php');
-
- ?>
--- BasicBlogMVC/app/init.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $db = require_once(PATH . '/app/database.php');
-
-  // Helper
-  foreach(glob(PATH . '/app/helper/*.php') as $helperFile){
-    require_once($helperFile);
-  }
- ?>
